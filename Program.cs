1559f36166f74ce87a8f0fa9e31dfd385d86c7a4
Ali-Karamiani <134929815+AliKaramiani@users.noreply.github.com>
2024-04-22 17:13:20 +0330

    exercise 3

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int sum;
+do
+{
+    sum = 0;
+    System.Console.WriteLine("please enter x");
+    int x = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("please enter y");
+    int y = Convert.ToInt32(Console.ReadLine());
+    for (var i = 1; i <= y; i++)
+    {
+        sum = sum + x;
+
+    }
+    System.Console.WriteLine(sum);
+} while (sum != 0);
+
